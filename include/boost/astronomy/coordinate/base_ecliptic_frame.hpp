2598d7e416503a8d1f06bac153569e95de0dc85e
Eslam Tarek <36302669+Solariii@users.noreply.github.com>
2020-02-15 23:20:31 +0200

    Update base_ecliptic_frame.hpp

--- include/boost/astronomy/coordinate/base_ecliptic_frame.hpp
+++ include/boost/astronomy/coordinate/base_ecliptic_frame.hpp
@@ -1,3 +1,8 @@
+
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef BOOST_ASTRONOMY_COORDINATE_BASE_ECLIPTIC_FRAME_HPP
 #define BOOST_ASTRONOMY_COORDINATE_BASE_ECLIPTIC_FRAME_HPP
 
