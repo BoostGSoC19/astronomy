ea2be0b21461e9c184f5b7ba0d016d5bc035182f
Eslam Tarek <36302669+Solariii@users.noreply.github.com>
2020-02-15 23:26:06 +0200

    Update heliocentric.hpp

--- include/boost/astronomy/coordinate/heliocentric.hpp
+++ include/boost/astronomy/coordinate/heliocentric.hpp
@@ -1,3 +1,8 @@
+
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef BOOST_ASTRONOMY_COORDINATE_HELIOCENTRIC_HPP
 #define BOOST_ASTRONOMY_COORDINATE_HELIOCENTRIC_HPP
 
