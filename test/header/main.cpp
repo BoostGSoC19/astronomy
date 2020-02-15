847e65ba1305b4b6ce441b9b70f89fb174504d17
Eslam Tarek <36302669+Solariii@users.noreply.github.com>
2020-02-15 23:41:16 +0200

    Update main.cpp

--- test/header/main.cpp
+++ test/header/main.cpp
@@ -1,3 +1,8 @@
+
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #define BOOST_ASTRONOMY_TEST_INCLUDE_HEADER() <boost/astronomy/BOOST_ASTRONOMY_TEST_HEADER>
 
 #include BOOST_ASTRONOMY_TEST_INCLUDE_HEADER()
