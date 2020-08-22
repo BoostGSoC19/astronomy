af00cc7092ad5c584b6ee423f38b819d644d798d
Zyro9922 <alihasan9922@gmail.com>
2020-08-22 19:46:23 +0530

    PI macro changed to constexpr. Updated struct names. Renamed variables in Obiquity of Ecliptic

--- include/boost/astronomy/coordinate/utility/utility.hpp
+++ include/boost/astronomy/coordinate/utility/utility.hpp
@@ -28,7 +28,7 @@ file License.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/astronomy/coordinate/coord_sys/coord_sys.hpp>
 
-#define PI        3.14159265358979323846264338327950288
+constexpr long double PI = 3.141592653589793238462643383279502884L;
 
 using namespace std;
 namespace bu = boost::units;
@@ -50,14 +50,14 @@ template
         typename Angle = bu::quantity<bu::si::plane_angle, CoordinateType>,
         typename ElementType = double
     >
-struct col_vec
+struct column_vector
 {
  public:
   matrix<ElementType> vec = matrix<ElementType>(3, 1);
 
-  col_vec() {}
+  column_vector() {}
 
-  col_vec(Angle u, Angle v){
+  column_vector(Angle u, Angle v){
     double _u = static_cast<bu::quantity<bu::si::plane_angle>>(u).value();
     double _v = static_cast<bu::quantity<bu::si::plane_angle>>(v).value();
 
@@ -77,14 +77,14 @@ struct col_vec
 
 template
     <typename ElementType = double>
-struct ha_dec_hor
+struct hour_angle_declination_horizon
 {
  public:
   matrix<ElementType> conv = matrix<ElementType>(3, 3);
 
-  ha_dec_hor() {}
+  hour_angle_declination_horizon() {}
 
-  ha_dec_hor(ElementType phi){
+  hour_angle_declination_horizon(ElementType phi){
     conv(0,0) = -std::sin(phi);
     conv(0,1) = 0;
     conv(0,2) = std::cos(phi);
@@ -107,14 +107,14 @@ struct ha_dec_hor
 
 template
     <typename ElementType = double>
-struct ha_dec_ra_dec
+struct hour_angle_declination_right_ascension_declination
 {
  public:
   matrix<ElementType> conv = matrix<ElementType>(3, 3);
 
-  ha_dec_ra_dec() {}
+  hour_angle_declination_right_ascension_declination() {}
 
-  ha_dec_ra_dec(ElementType ST){
+  hour_angle_declination_right_ascension_declination(ElementType ST){
     conv(0,0) = std::cos(ST);
     conv(0,1) = std::sin(ST);
     conv(0,2) = 0;
@@ -137,14 +137,14 @@ struct ha_dec_ra_dec
 
 template
     <typename ElementType = double>
-struct ecliptic_to_ra_dec
+struct ecliptic_to_right_ascension_declination
 {
  public:
   matrix<ElementType> conv = matrix<ElementType>(3, 3);
 
-  ecliptic_to_ra_dec() {}
+  ecliptic_to_right_ascension_declination() {}
 
-  ecliptic_to_ra_dec(ElementType obliquity){
+  ecliptic_to_right_ascension_declination(ElementType obliquity){
     conv(0,0) = 1;
     conv(0,1) = 0;
     conv(0,2) = 0;
@@ -167,14 +167,14 @@ struct ecliptic_to_ra_dec
 
 template
     <typename ElementType = double>
-struct ra_dec_to_ecliptic
+struct right_ascension_declination_to_ecliptic
 {
  public:
   matrix<ElementType> conv = matrix<ElementType>(3, 3);
 
-  ra_dec_to_ecliptic() {}
+  right_ascension_declination_to_ecliptic() {}
 
-  ra_dec_to_ecliptic(ElementType obliquity){
+  right_ascension_declination_to_ecliptic(ElementType obliquity){
     conv(0,0) = 1;
     conv(0,1) = 0;
     conv(0,2) = 0;
@@ -197,12 +197,12 @@ struct ra_dec_to_ecliptic
 
 template
     <typename ElementType = double>
-struct galactic_to_ra_dec
+struct galactic_to_right_ascension_declination
 {
  public:
   matrix<ElementType> conv = matrix<ElementType>(3, 3);
 
-  galactic_to_ra_dec(){
+  galactic_to_right_ascension_declination(){
     conv(0,0) = -0.0669887;
     conv(0,1) = -0.8727558;
     conv(0,2) = -0.4835389;
@@ -225,12 +225,12 @@ struct galactic_to_ra_dec
 
 template
     <typename ElementType = double>
-struct ra_dec_to_galactic
+struct right_ascension_declination_to_galactic
 {
  public:
   matrix<ElementType> conv = matrix<ElementType>(3, 3);
 
-  ra_dec_to_galactic()
+  right_ascension_declination_to_galactic()
   {
     conv(0,0) = -0.0669887;
     conv(0,1) = 0.8727558;
@@ -265,11 +265,11 @@ struct extract_coordinates{
  public:
   extract_coordinates(){}
 
-  explicit extract_coordinates(matrix<double> col_vec)
+  explicit extract_coordinates(matrix<double> column_vector)
   {
-    double m = col_vec(0,0);
-    double n = col_vec(1,0);
-    double p = col_vec(2,0);
+    double m = column_vector(0,0);
+    double n = column_vector(1,0);
+    double p = column_vector(2,0);
 
     theta = atan2(n,m);
     phi = asin(p);
@@ -287,17 +287,13 @@ struct obliquity_of_ecliptic{
  public:
   obliquity_of_ecliptic(date d)
   {
-    double JD = d.julian_day();
+    double julian_date = d.julian_day();
 
-    double S = JD - 2451545.0;
+    double modified_julian_date = julian_date - 2451545.0;
 
-    double T = S / 36525.0;
+    double julian_centuries = modified_julian_date / 36525.0;
 
-    double DE = 46.815 * T + (0.0006 - 0.00181 * T) * T * T;
-
-    DE = DE / 3600.0;
-
-    double e_degrees =  23.439292 - DE;
+    double e_degrees =  23.439292 - (46.815 * julian_centuries + (0.0006 - 0.00181 * julian_centuries) * julian_centuries * julian_centuries) / 3600.0;
 
     e = (e_degrees * PI / 180.0) * bu::si::radian;
   }
